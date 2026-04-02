430ed0c5cc44f138d5cffa78dbed9ecd3eff0ed0
Sandbox user <tools@notanumber.com>
2026-04-02 02:01:01 +0000

    Error: Failed to run sql query: ERROR: 42601: syntax error at or near ".." LINE 2: ALTER TABLE events ADD COLUMN IF NOT EXISTS weather_condition TEXT... ^

--- mobile/supabase-events-weather-migration.sql
+++ mobile/supabase-events-weather-migration.sql
@@ -6,6 +6,6 @@
 
 ALTER TABLE events ADD COLUMN IF NOT EXISTS weather_temp INTEGER;
 ALTER TABLE events ADD COLUMN IF NOT EXISTS weather_condition TEXT
-  CHECK (weather_condition IN ('sunny', 'partly_cloudy', 'cloudy', 'rain', 'snow', 'indoor'));
+  CHECK (weather_condition IN ('sunny','partly_cloudy','cloudy','rain','snow','indoor'));
 ALTER TABLE events ADD COLUMN IF NOT EXISTS weather_auto_fetched BOOLEAN DEFAULT false;
 ALTER TABLE events ADD COLUMN IF NOT EXISTS weather_is_forecast BOOLEAN DEFAULT false;
